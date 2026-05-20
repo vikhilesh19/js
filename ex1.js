c286d7910cc443ab5f349c432db1f364661007f7
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-05-20 14:48:53 +0530

    ex1

---
+++ ex1.js
@@ -0,0 +1,14 @@
+alert('Hello');
+console.log('Hai');
+console.log(3+6);
+console.warn('This is a warning');
+console.error('This is an error');
+//single line comment
+/* 
+ This
+ is
+ a 
+ multi
+ line
+ comment
+*/
