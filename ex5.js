c9aec5f89fa845ae9972d8d7aa8ea9c93b4db4cf
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-05-21 14:36:53 +0530

    ex5

--- ex5.js
+++ ex5.js
@@ -1,5 +1,15 @@
 //Ternary operator.
-let age;
-age=32;
-let result= age >= 18 ? 'Adult' : 'Minor'; 
+let age1;
+age1=32;
+let result= age1 >= 18 ? 'Adult' : 'Minor'; 
 console.log(result);
+
+//Guard operator.
+let age2=0;
+let finalage=age2||18;
+console.log(finalage);
+
+//Default parameters.
+let age3;
+let fage=age3??18;
+console.log(fage);
